7825d37fa79487c8c722c160c5d8a9c56ecfa3f7
NicoAversa <blackcatdsw@gmail.com>
2021-06-22 23:22:18 -0300

    Reentrega desafio clase 5

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -2,14 +2,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
 import { faMinusSquare } from '@fortawesome/free-solid-svg-icons'
 import './itemCount.css'
-import React from 'react'
-export const ItemCount = ()=>{
+import { useState } from 'react'
+
+export const ItemCount = ({stock, initial})=>{
 
     const iconPlus=<FontAwesomeIcon icon={faPlusSquare}/>
     const iconMinus=<FontAwesomeIcon icon={faMinusSquare}/>
-    const [Stock, setStock] = React.useState(5)
-    const [initial, setInitial] = React.useState(1)
-    const [onAdd, setOnAdd] = React.useState()
+    
+    const [Stock, setStock] = useState(5)
+    const [Initial, setInitial] = useState(1)
+    const [onAdd, setOnAdd] = useState(Initial)
+    
+    function plus(){
+        let newAdd = parseInt(onAdd) +1
+        setOnAdd(newAdd)
+    }
+
+    function minus(){
+        let newAdd = parseInt(onAdd) -1
+        setOnAdd(newAdd)
+    }
+
+    function addOk(){
+        if(onAdd > 0){
+            alert ("Agregaste " + " " + onAdd + " " + "Productos a tu carro")
+        } else {
+            alert ("No se agrego ningun producto a tu carro")
+        }
+    }
 
     return (
         <count>
@@ -17,13 +37,13 @@ export const ItemCount = ()=>{
         <div className='container'>
         <br/>
         <div className='count'>
-        <button id='btnMinus' href='#' onClick={()=>{if (initial>1) { document.querySelector('#btnMinus').disabled=false; document.querySelector('#btnAdd').disabled=false; setInitial(initial - 1)} else {document.querySelector('#btnMinus').disabled=true}}}><i>{iconMinus}</i></button>
-        <label className='contador'>{initial}</label>
-        <button id='btnAdd' href='#' onClick={()=>{ if(Stock>initial) { document.querySelector('#btnMinus').disabled=false; document.querySelector('#btnAdd').disabled=false;setInitial(initial + 1); } else{  document.querySelector('#btnAdd').disabled=true; alert('Supera cantidad en Stock') } }}><i>{iconPlus}</i></button>
+        <button id='btnMinus' onClick={()=>minus()} disabled={onAdd <= Initial}><i>{iconMinus}</i></button>
+        <label className='contador'>{onAdd}</label>
+        <button id='btnAdd' onClick={()=>plus()} disabled={onAdd>=Stock}><i>{iconPlus}</i></button>
         </div>
         <br/>
         <p>Stock Disponible:<b>{Stock}</b></p>
-        <button className='btnCarrito'>Agregar al carrito</button></div>
+        <button className='btnCarrito' onClick={()=>addOk()}>Agregar al carrito</button></div>
     </div>
         </count>
     )
